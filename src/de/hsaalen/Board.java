76a88b5444b60f23dc0c30f16cd758244889b10d
PB4ng <patrickkellner002@gmail.com>
2024-12-03 20:59:48 +0100

    InitialCommit

--- src/de/hsaalen/Board.java
+++ src/de/hsaalen/Board.java
@@ -1,4 +1,4 @@
-package com.zetcode;
+package de.hsaalen;
 
 import java.awt.Color;
 import java.awt.Dimension;
--- src/de/hsaalen/Snake.java
+++ src/de/hsaalen/Snake.java
@@ -1,4 +1,4 @@
-package com.zetcode;
+package de.hsaalen;
 
 import java.awt.EventQueue;
 import javax.swing.JFrame;
