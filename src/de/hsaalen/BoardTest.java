8efa0d213e39932ed3c6043949bf208a557d3759
Nici4001 <nicole.mayer04@gmail.com>
2024-12-05 22:00:46 +0100

    Aufgabe 2a) Unittests: for test_maximum_tile_index_x and test_maximum_tile_index_y

--- src/de/hsaalen/BoardTest.java
+++ src/de/hsaalen/BoardTest.java
@@ -4,6 +4,21 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BoardTest {
+    @Test
+    public void  test_maximum_tile_index_x()
+    {
+        Board board = new Board();
+        int maximum_tile_index_x = board.maximum_tile_index_x();
+        assertEquals( ( maximum_tile_index_x + 1 ) * board.tileSizeInPixels, board.widthInPixels );
+    }
+
+    @Test
+    public void  test_maximum_tile_index_y()
+    {
+        Board board = new Board();
+        int maximum_tile_index_y = board.maximum_tile_index_y();
+        assertEquals( ( maximum_tile_index_y + 1 ) * board.tileSizeInPixels, board.heightInPixels );
+    }
 
     @Test
     public void testConcatenate() {
